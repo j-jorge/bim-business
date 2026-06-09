7ae03bcd7e99c143faa88a49d65c5466d09e5033
Julien Jorge <julien.jorge@stuff-o-matic.com>
2026-06-09 20:33:22 +0200

    [tests] Display the list of all failing tests at the end of run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -8,12 +8,14 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 . "$script_dir"/colors.sh
 
 fail_count=0
+failing=()
 
 while read -r test_script
 do
     if ! "$test_script" "$@"
     then
         fail_count=$((fail_count + 1))
+        failing+=("$test_script")
     fi
 done < <(find "$script_dir" -mindepth 2 -executable -name "*.sh")
 
@@ -22,7 +24,8 @@ then
     echo -e "${green}[======]$reset_color"
     echo -e "${green}[ PASS ]$reset_color All tests passed."
 else
-    echo -e "${red}[======]$reset_color $fail_count failures."
-    echo -e "${red}[ FAIL ]$reset_color $fail_count failures."
+    echo -e "${red}[======]$reset_color"
+    echo -e "${red}[ FAIL ]$reset_color $fail_count failures:"
+    printf '  %s\n' "${failing[@]}"
     exit 1
 fi
